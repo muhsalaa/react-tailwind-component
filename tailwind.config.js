1eb6654b35e48052c2a5a7bb2c75a982769680fc
M Salma <principiorum@pm.me>
2023-07-12 08:42:51 +0700

    fix styling 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [".src/components/**/*.{ts,tsx}"],
+  content: ["./src/components/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
